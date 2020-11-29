d5f5522686b65ee14b4c1461bea82f2037be7de1
zoudmbean <zoudmbean@163.com>
2020-11-30 07:34:49 +0800

    end

--- src/components/ShopCar/ShopCar.vue
+++ src/components/ShopCar/ShopCar.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="shopcart">
       <div class="content">
-        <div class="content-left">
+        <div class="content-left" @click="toggleShow">
           <div class="logo-wrapper">
             <div class="logo" :class="{highlight: totalCount}">
               <i class="iconfont icon-shopping_cart" :class="{highlight: totalCount}"></i>
@@ -18,17 +18,48 @@
           </div>
         </div>
       </div>
+      <transition name="move">
+        <div class="shopcart-list" v-show="listShow">
+          <div class="list-header">
+            <h1 class="title">购物车</h1>
+            <span class="empty" @click="clearCart">清空</span>
+          </div>
+          <div class="list-content">
+            <ul>
+              <li class="food" v-for="(food, index) in cartFoods" :key="index">
+                <span class="name">{{food.name}}</span>
+                <div class="price"><span>￥{{food.price}}</span></div>
+                <div class="cartcontrol-wrapper">
+                  <CarControl :food="food"/>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </transition>
+
     </div>
+    <div class="list-mask" v-show="listShow" @click="toggleShow"></div>
   </div>
 </template>
 
 <script>
-  import {mapState,mapGetters} from 'vuex'
+  import { MessageBox } from 'mint-ui'
+  import BScroll from 'better-scroll'
+  import {mapState, mapGetters} from 'vuex'
+  import CarControl from '../CarControl/CarControl.vue'
+
   export default {
-    name: 'ShopCar',
-    computed:{
-      ...mapState(['info']),
-      ...mapGetters(['totalCount','totalPrice']),
+    name:'ShopCar',
+    data () {
+      return {
+        isShow: false
+      }
+    },
+
+    computed: {
+      ...mapState(['cartFoods', 'info']),
+      ...mapGetters(['totalCount', 'totalPrice']),
       payClass () {
         const {totalPrice} = this
         const {minPrice} = this.info
@@ -46,11 +77,54 @@
           return '结算'
         }
       },
+
+      listShow () {
+        // 如果总数量为0, 直接不显示
+        if(this.totalCount===0) {
+          this.isShow = false
+          return false
+        }
+
+        if(this.isShow) {
+          this.$nextTick(() => {
+            // 实现BScroll的实例是一个单例
+            if(!this.scroll) {
+              this.scroll = new BScroll('.list-content', {
+                click: true
+              })
+            } else {
+              this.scroll.refresh() // 让滚动条刷新一下: 重新统计内容的高度
+            }
+
+          })
+        }
+
+        return this.isShow
+      }
+    },
+
+
+    methods: {
+      toggleShow () {
+        // 只有当总数量大于0时切换
+        if(this.totalCount>0) {
+          this.isShow = !this.isShow
+        }
+      },
+
+      clearCart () {
+        MessageBox.confirm('确定清空购物车吗?').then(action => {
+          this.$store.dispatch('clearCart')
+        }, () => {});
+      }
+    },
+    components: {
+      CarControl
     }
   }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
+<style lang="stylus" rel="stylesheet/stylus" scoped>
   @import "../../common/stylus/mixins.styl"
 
   .shopcart
